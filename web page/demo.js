5f50cc2c2c4fcbfc9d30a32146c33db5d99a0a19
sonali <sonali21112001@gmail.com>
2023-01-02 10:56:22 +0530

    react

--- web page/demo.js
+++ web page/demo.js
@@ -1,23 +1,24 @@
 'use strict';
-(function(){
+(function () {
 
-    function Person(firstName, lastName){
+    function Person(firstName, lastName) {
+
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
     Person.prototype.age = 29;
 
-    var jin = new Person('jin','Cooper');
-    var sofia = new Person('sofia','Cooper');
+    var jin = new Person('jin', 'Cooper');
+    var sofia = new Person('sofia', 'Cooper');
 
-    Person.prototype - {age: 18};
+    Person.prototype - { age: 18 };
 
-    let kris = new Person('kris','Cooper');
-    
+    let kris = new Person('kris', 'Cooper');
+
     display(Person.prototype);
     display(jin.age);
     display(sofia.age);
-    display =(kris.age);
+    display(kris.age);
 
-}) ();
+})();
